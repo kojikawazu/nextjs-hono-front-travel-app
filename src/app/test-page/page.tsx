db4aeca0511c82ceb8c28ed953f3423326085872
kojikawazu <nanairo7.daydream21@gmail.com>
2024-07-11 00:39:58 +0900

    テストページデバッグ

--- src/app/test-page/page.tsx
+++ src/app/test-page/page.tsx
@@ -4,8 +4,10 @@ import React from 'react';
 import useSWR from 'swr';
 
 const TestPage = () => {
+    const backend_url = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
+
     const { data: text } = useSWR(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/test-route`, 
+        `${backend_url}/test-route`, 
         (url) => fetch(url).then((res) => res.text())
     );
 
