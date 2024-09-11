b1258d2d7a011c55b04f136e8502af98e6d0dcbb
kojikawazu <nanairo7.daydream21@gmail.com>
2024-09-11 22:25:05 +0900

    ログイン後のページ不具合解消

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
 export default function Home() {
+    const router = useRouter();
+
+    useEffect(() => {
+        if (window.location.pathname === '/') {
+            router.push('/projects');
+        }
+    }, [router]);
+
     return <main className="">Hello</main>;
 }
